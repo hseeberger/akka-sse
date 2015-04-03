015facc8de4a11d9d5c36d6786f80a7d40090181
Heiko Seeberger <mail@heikoseeberger.de>
2015-04-03 17:55:55 +0200

    Rename event to eventType

--- akka-sse/src/main/scala/de/heikoseeberger/akkasse/ServerSentEvent.scala
+++ akka-sse/src/main/scala/de/heikoseeberger/akkasse/ServerSentEvent.scala
@@ -24,24 +24,24 @@ object ServerSentEvent {
   /**
    * Creates a [[ServerSentEvent]] with defined event type.
    * @param data data which may span multiple lines
-   * @param event event type, must not contain \n or \r
+   * @param eventType event type, must not contain \n or \r
    */
-  def apply(data: String, event: String) = new ServerSentEvent(data, Some(event))
+  def apply(data: String, eventType: String): ServerSentEvent = new ServerSentEvent(data, Some(eventType))
 
   /**
    * Java API.
    * Creates a [[ServerSentEvent]] without event type.
    * @param data data which may span multiple lines
    */
-  def create(data: String) = new ServerSentEvent(data)
+  def create(data: String): ServerSentEvent = new ServerSentEvent(data)
 
   /**
    * Java API.
    * Creates a [[ServerSentEvent]] with event type.
    * @param data data which may span multiple lines
-   * @param event event type, must not contain \n or \r
+   * @param eventType event type, must not contain \n or \r
    */
-  def create(data: String, event: String) = new ServerSentEvent(data, Some(event))
+  def create(data: String, eventType: String): ServerSentEvent = new ServerSentEvent(data, Some(eventType))
 
   // Public domain algorithm: http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
   // We want powers of two both because they typically work better with the allocator,
@@ -58,7 +58,7 @@ object ServerSentEvent {
 }
 
 /**
- * Reprsentation of a Server-Sent Event.
+ * Representation of a Server-Sent Event.
  * @param data data which may span multiple lines
  * @param eventType optional event type, must not contain \n or \r
  */
@@ -66,7 +66,14 @@ final case class ServerSentEvent(data: String, eventType: Option[String] = None)
 
   import ServerSentEvent._
 
-  require(eventType.forall(_.forall(c => c != '\n' && c != '\r')), "Event must not contain \\n or \\r!")
+  require(eventType.forall(_.forall(c => c != '\n' && c != '\r')), "Event type must not contain \\n or \\r!")
+
+  /**
+   * Convert to an `akka.util.ByteString`
+   * according to the [[http://www.w3.org/TR/eventsource/#event-stream-interpretation SSE specification]].
+   * @return message converted to UTF-8 encoded `akka.util.ByteString`
+   */
+  def toByteString: ByteString = ByteString(toString, "UTF-8")
 
   /**
    * Convert to a `java.lang.String`
@@ -85,8 +92,8 @@ final case class ServerSentEvent(data: String, eventType: Option[String] = None)
         }
       builder.append(label)
       addLine(index) match {
-        case -1    => builder.append('\n')
-        case index => addLines(builder, label, s, index)
+        case -1 => builder.append('\n')
+        case i  => addLines(builder, label, s, i)
       }
     }
     def addData(builder: StringBuilder) = addLines(builder, "data:", data, 0).append('\n')
@@ -100,11 +107,4 @@ final case class ServerSentEvent(data: String, eventType: Option[String] = None)
     val builder = new StringBuilder(nextPowerOfTwoBiggerThan(8 + data.length + eventType.fold(0)(_.length + 7)))
     addData(addEvent(builder)).toString()
   }
-
-  /**
-   * Convert to an `akka.util.ByteString`
-   * according to the [[http://www.w3.org/TR/eventsource/#event-stream-interpretation SSE specification]].
-   * @return message converted to UTF-8 encoded `akka.util.ByteString`
-   */
-  def toByteString = ByteString(toString, "UTF-8")
 }
