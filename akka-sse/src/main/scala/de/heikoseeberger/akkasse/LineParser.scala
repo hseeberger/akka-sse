ff23b908a58978f593f37bd148c72ff55b9a6fdc
Heiko Seeberger <mail@heikoseeberger.de>
2016-01-27 18:10:29 +0100

    Fix parse lines (closes #78)

--- akka-sse/src/main/scala/de/heikoseeberger/akkasse/LineParser.scala
+++ akka-sse/src/main/scala/de/heikoseeberger/akkasse/LineParser.scala
@@ -32,31 +32,28 @@ private final class LineParser(maxLineSize: Int) extends GraphStage[FlowShape[By
   override val shape = FlowShape(Inlet[ByteString]("LineParser.in"), Outlet[String]("LineParser.out"))
 
   override def createLogic(inheritedAttributes: Attributes) = new GraphStageLogic(shape) {
-    import shape.{ in, out }
-    import LineParser.{ CR, LF }
+    import LineParser._
+    import shape._
 
     setHandler(in, new InHandler {
       private var buffer = ByteString.empty
-
-      @tailrec private def parseLines(
-        buf: ByteString,
-        from: Int = 0,
-        at: Int = 0,
-        parsed: Vector[String] = Vector.empty
-      ): (ByteString, Vector[String]) =
-        if (at >= buf.length) (buf.drop(from), parsed)
-        else buf(at) match {
-          case CR if buf(math.min(at + 1, buf.length - 1)) == LF => // Lookahead for LF after CR
-            parseLines(buf, at + 2, at + 2, parsed :+ buf.slice(from, at).utf8String)
-          case CR if buf(math.max(at - 1, 0)) == LF => // Ignore first CR after a CRLF sequence
-            parseLines(buf, at + 1, at + 1, parsed)
-          case CR | LF => // a CR or LF means we found a new slice
-            parseLines(buf, at + 1, at + 1, parsed :+ buf.slice(from, at).utf8String)
-          case _ => // for other input, simply advance
-            parseLines(buf, from, at + 1, parsed)
-        }
-
       override def onPush() = {
+        @tailrec
+        def parseLines(
+          buffer: ByteString,
+          from: Int = 0,
+          at: Int = 0,
+          parsedLines: Vector[String] = Vector.empty
+        ): (ByteString, Vector[String]) =
+          if (at >= buffer.length) (buffer.drop(from), parsedLines)
+          else buffer(at) match {
+            case CR if buffer(math.min(at + 1, buffer.length - 1)) == LF => // Lookahead for LF after CR
+              parseLines(buffer, at + 2, at + 2, parsedLines :+ buffer.slice(from, at).utf8String)
+            case CR | LF => // a CR or LF means we found a new slice
+              parseLines(buffer, at + 1, at + 1, parsedLines :+ buffer.slice(from, at).utf8String)
+            case _ => // for other input, simply advance
+              parseLines(buffer, from, at + 1, parsedLines)
+          }
         buffer = parseLines(buffer ++ grab(in)) match {
           case (remaining, _) if remaining.size > maxLineSize =>
             failStage(new IllegalStateException(s"maxLineSize of $maxLineSize exceeded!"))
