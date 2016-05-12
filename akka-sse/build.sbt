dc202b0dad0f2f71fe8bd1a5aabd1b483542e1b6
Heiko Seeberger <hseeberger@users.noreply.github.com>
2016-05-12 08:34:55 -0400

    Use akka-stream-contrib instead of commons-akka-stream (#110)

--- akka-sse/build.sbt
+++ akka-sse/build.sbt
@@ -2,12 +2,11 @@ name := "akka-sse"
 
 libraryDependencies ++= Vector(
   Library.akkaHttp,
-  Library.commonsAkkaStream,
-  Library.akkaHttpTestkit   % "test",
-  Library.akkaTestkit       % "test",
-  Library.junit             % "test",
-  Library.scalaCheck        % "test",
-  Library.scalaTest         % "test"
+  Library.akkaStreamContrib,
+  Library.akkaHttpTestkit % "test",
+  Library.junit           % "test",
+  Library.scalaCheck      % "test",
+  Library.scalaTest       % "test"
 )
 
 initialCommands := """|import de.heikoseeberger.akkasse._""".stripMargin
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,20 +1,19 @@
 import sbt._
 
 object Version {
-  final val Akka        = "2.4.4"
-  final val CommonsAkka = "0.4.0"
-  final val Junit       = "4.12"
-  final val Scala       = "2.11.8"
-  final val ScalaCheck  = "1.12.5"
-  final val ScalaTest   = "2.2.6"
+  final val Akka              = "2.4.4"
+  final val AkkaStreamContrib = "0.1"
+  final val Junit             = "4.12"
+  final val Scala             = "2.11.8"
+  final val ScalaCheck        = "1.12.5"
+  final val ScalaTest         = "2.2.6"
 }
 
 object Library {
   val akkaHttp          = "com.typesafe.akka" %% "akka-http-experimental" % Version.Akka
   val akkaHttpTestkit   = "com.typesafe.akka" %% "akka-http-testkit"      % Version.Akka
   val akkaStream        = "com.typesafe.akka" %% "akka-stream"            % Version.Akka
-  val akkaTestkit       = "com.typesafe.akka" %% "akka-testkit"           % Version.Akka
-  val commonsAkkaStream = "de.heikoseeberger" %% "commons-akka-stream"    % Version.CommonsAkka
+  val akkaStreamContrib = "com.typesafe.akka" %% "akka-stream-contrib"    % Version.AkkaStreamContrib
   val junit             = "junit"             %  "junit"                  % Version.Junit
   val scalaCheck        = "org.scalacheck"    %% "scalacheck"             % Version.ScalaCheck
   val scalaTest         = "org.scalatest"     %% "scalatest"              % Version.ScalaTest
