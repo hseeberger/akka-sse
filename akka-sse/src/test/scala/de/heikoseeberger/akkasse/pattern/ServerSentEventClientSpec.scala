2b3755c0343a8cfd28ec32f7a5238e64a14755b4
Heiko Seeberger <hseeberger@users.noreply.github.com>
2016-05-12 09:49:18 -0400

    Apply initial last event ID if event ID absent (#111)

--- akka-sse/src/test/scala/de/heikoseeberger/akkasse/pattern/ServerSentEventClientSpec.scala
+++ akka-sse/src/test/scala/de/heikoseeberger/akkasse/pattern/ServerSentEventClientSpec.scala
@@ -19,14 +19,15 @@ package pattern
 
 import akka.Done
 import akka.actor.ActorDSL.actor
-import akka.actor.{ Actor, ActorLogging, ActorRef, Status }
+import akka.actor.{ Actor, ActorLogging, Status }
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes.BadRequest
 import akka.http.scaladsl.model.{ HttpEntity, HttpResponse, Uri }
-import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.server.{ Directives, Route }
 import akka.pattern.pipe
 import akka.stream.scaladsl.{ Sink, Source }
 import akka.stream.{ ActorMaterializer, ThrottleMode }
+import akka.testkit.TestDuration
 import de.heikoseeberger.akkasse.MediaTypes.`text/event-stream`
 import de.heikoseeberger.akkasse.headers.`Last-Event-ID`
 import java.net.InetSocketAddress
@@ -38,10 +39,10 @@ object ServerSentEventClientSpec {
 
   object Server {
 
-    case object Start
-    case object Stop
+    private case object Bind
+    private case object Unbind
 
-    private def route(server: ActorRef) = {
+    def route(setEventId: Boolean) = {
       import Directives._
       import EventStreamMarshalling._
       get {
@@ -51,7 +52,7 @@ object ServerSentEventClientSpec {
             complete {
               Source
                 .fromIterator(() => Iterator.from(fromSeqNo).take(2))
-                .map(toServerSentEvent)
+                .map(toServerSentEvent(setEventId))
             }
           } catch {
             case e: NumberFormatException =>
@@ -65,35 +66,35 @@ object ServerSentEventClientSpec {
     }
   }
 
-  class Server(address: String, port: Int) extends Actor with ActorLogging {
+  class Server(address: String, port: Int, route: Route, isInstable: Boolean) extends Actor with ActorLogging {
     import Server._
     import context.dispatcher
 
     private implicit val mat = ActorMaterializer()
 
-    self ! Start
+    self ! Bind
 
-    override def receive = inactive
+    override def receive = unbound
 
-    private def inactive: Receive = {
-      case Start =>
-        Http(context.system).bindAndHandle(route(self), address, port).pipeTo(self)
+    private def unbound: Receive = {
+      case Bind =>
+        Http(context.system).bindAndHandle(route, address, port).pipeTo(self)
         context.become(binding)
     }
 
     private def binding: Receive = {
       case serverBinding @ Http.ServerBinding(socketAddress) =>
         log.info("Listening on {}", socketAddress)
-        context.system.scheduler.scheduleOnce(1500.milliseconds, self, Stop)
-        context.become(active(serverBinding))
+        context.system.scheduler.scheduleOnce(1500.milliseconds, self, Unbind)
+        context.become(bound(serverBinding))
 
       case Status.Failure(cause) =>
         log.error(cause, s"Can't bind to {}:{}!", address, port)
         context.stop(self)
     }
 
-    private def active(serverBinding: Http.ServerBinding): Receive = {
-      case Stop =>
+    private def bound(serverBinding: Http.ServerBinding): Receive = {
+      case Unbind =>
         serverBinding.unbind().map(_ => Done).pipeTo(self)
         context.become(unbinding(serverBinding.localAddress))
     }
@@ -101,8 +102,8 @@ object ServerSentEventClientSpec {
     private def unbinding(socketAddress: InetSocketAddress): Receive = {
       case Done =>
         log.info("Stopped listening on {}", socketAddress)
-        context.system.scheduler.scheduleOnce(500.milliseconds, self, Start)
-        context.become(inactive)
+        context.system.scheduler.scheduleOnce(500.milliseconds, self, Bind)
+        context.become(unbound)
 
       case Status.Failure(cause) =>
         log.error(cause, s"Can't unbind from {}!", socketAddress)
@@ -110,9 +111,9 @@ object ServerSentEventClientSpec {
     }
   }
 
-  private def toServerSentEvent(n: Int) = {
+  private def toServerSentEvent(setEventId: Boolean)(n: Int) = {
     val id = n.toString
-    ServerSentEvent(id, id = Some(id))
+    if (setEventId) ServerSentEvent(id, id = Some(id)) else ServerSentEvent(id)
   }
 }
 
@@ -120,21 +121,37 @@ class ServerSentEventClientSpec extends BaseSpec {
   import ServerSentEventClientSpec._
 
   "ServerSentEventClient" should {
-    "communicate with the HTTP server correctly" in {
+    "communicate correctly with an instable HTTP server" in {
       val host = "localhost"
       val port = 9999
-      val server = actor(new Server(host, port))
+      val server = actor(new Server(host, port, Server.route(setEventId = true), isInstable = true))
       val nrOfSamples = 20
       val handler = Sink.fold[Vector[ServerSentEvent], ServerSentEvent](Vector.empty)(_ :+ _)
-      val events = ServerSentEventClient(Uri(s"http://$host:$port"), handler)
-        .throttle(2, 1.second, 1, ThrottleMode.Shaping)
+      val events = ServerSentEventClient(Uri(s"http://$host:$port"), handler, Some("2"))
+        .throttle(1, 500.milliseconds, 1, ThrottleMode.Shaping)
         .mapAsync(1)(identity)
         .mapConcat(identity)
         .take(nrOfSamples)
         .runFold(Vector.empty[ServerSentEvent])(_ :+ _)
-      Await.result(events, 20.seconds) shouldBe 1.to(nrOfSamples).map(toServerSentEvent)
+      val expected = Vector.iterate(3, nrOfSamples)(_ + 1).map(toServerSentEvent(setEventId = true))
+      Await.result(events, 20.seconds.dilated) shouldBe expected
+      system.stop(server)
+    }
+
+    "apply the initial last event if the server doesn't set the event id" in {
+      val host = "localhost"
+      val port = 9998
+      val server = actor(new Server(host, port, Server.route(setEventId = false), isInstable = false))
+      val nrOfSamples = 20
+      val handler = Sink.fold[Vector[ServerSentEvent], ServerSentEvent](Vector.empty)(_ :+ _)
+      val events = ServerSentEventClient(Uri(s"http://$host:$port"), handler, Some("2"))
+        .mapAsync(1)(identity)
+        .mapConcat(identity)
+        .take(nrOfSamples)
+        .runFold(Vector.empty[ServerSentEvent])(_ :+ _)
+      val expexted = Vector.tabulate(20)(n => n % 2 + 3).map(toServerSentEvent(setEventId = false))
+      Await.result(events, 3.seconds.dilated) shouldBe expexted
       system.stop(server)
     }
   }
 }
-
