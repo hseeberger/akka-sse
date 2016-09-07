7e41d321ea70824696423210248f38da9b6efe74
Heiko Seeberger <hseeberger@users.noreply.github.com>
2016-09-07 12:50:57 +0200

    Upgrade to Akka 2.4.10 (#134)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Version {
-  final val Akka              = "2.4.9"
+  final val Akka              = "2.4.10"
   final val AkkaStreamContrib = "0.2"
   final val Junit             = "4.12"
   final val Scala             = "2.11.8"
