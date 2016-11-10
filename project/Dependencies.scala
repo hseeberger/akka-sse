785ca3ef74fe10ef0eb870a9bdd9f7da6e6fe2bd
Heiko Seeberger <hseeberger@users.noreply.github.com>
2016-11-10 17:51:56 +0100

    Upgrade to Scala 2.12 and Akka HTTP 10 (#145)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,20 +1,21 @@
 import sbt._
 
 object Version {
-  final val Akka              = "2.4.11"
-  final val AkkaStreamContrib = "0.4"
+  final val Akka              = "2.4.12"
+  final val AkkaHttp          = "10.0.0-RC2"
+  final val AkkaStreamContrib = "0.5"
   final val Junit             = "4.12"
-  final val Scala             = "2.11.8"
+  final val Scala             = "2.12.0"
   final val ScalaCheck        = "1.13.4"
   final val ScalaTest         = "3.0.0"
 }
 
 object Library {
-  val akkaHttp          = "com.typesafe.akka" %% "akka-http-experimental" % Version.Akka
-  val akkaHttpTestkit   = "com.typesafe.akka" %% "akka-http-testkit"      % Version.Akka
-  val akkaStream        = "com.typesafe.akka" %% "akka-stream"            % Version.Akka
-  val akkaStreamContrib = "com.typesafe.akka" %% "akka-stream-contrib"    % Version.AkkaStreamContrib
-  val junit             = "junit"             %  "junit"                  % Version.Junit
-  val scalaCheck        = "org.scalacheck"    %% "scalacheck"             % Version.ScalaCheck
-  val scalaTest         = "org.scalatest"     %% "scalatest"              % Version.ScalaTest
+  val akkaHttp          = "com.typesafe.akka" %% "akka-http"           % Version.AkkaHttp
+  val akkaHttpTestkit   = "com.typesafe.akka" %% "akka-http-testkit"   % Version.AkkaHttp
+  val akkaStream        = "com.typesafe.akka" %% "akka-stream"         % Version.Akka
+  val akkaStreamContrib = "com.typesafe.akka" %% "akka-stream-contrib" % Version.AkkaStreamContrib
+  val junit             = "junit"             %  "junit"               % Version.Junit
+  val scalaCheck        = "org.scalacheck"    %% "scalacheck"          % Version.ScalaCheck
+  val scalaTest         = "org.scalatest"     %% "scalatest"           % Version.ScalaTest
 }
