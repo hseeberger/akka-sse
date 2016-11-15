327dc9656be320905dc0a7af018b96bebefcf5a0
Viktor Klang (√) <viktor.klang@gmail.com>
2016-11-15 12:05:55 +0100

    Reducing memory usage and indirection for LineParser (#152)

--- akka-sse/src/main/scala/de/heikoseeberger/akkasse/client/LineParser.scala
+++ akka-sse/src/main/scala/de/heikoseeberger/akkasse/client/LineParser.scala
@@ -35,62 +35,59 @@ private final class LineParser(maxLineSize: Int)
                                  Outlet[String]("LineParser.out"))
 
   override def createLogic(attributes: Attributes) =
-    new GraphStageLogic(shape) {
+    new GraphStageLogic(shape) with InHandler with OutHandler {
       import LineParser._
       import shape._
 
-      setHandler(in, new InHandler {
+      private var buffer = ByteString.empty
 
-        private var buffer = ByteString.empty
+      setHandlers(in, out, this)
 
-        override def onPush() = {
-          @tailrec
-          def parseLines(
-              bs: ByteString,
-              from: Int = 0,
-              at: Int = 0,
-              parsedLines: Vector[String] = Vector.empty
-          ): (ByteString, Vector[String]) =
-            if (at >= bs.length || (at == bs.length - 1 && bs(at) == CR))
-              (bs.drop(from), parsedLines)
-            else
-              bs(at) match {
-                // Lookahead for LF after CR
-                case CR if bs(at + 1) == LF =>
-                  parseLines(bs,
-                             at + 2,
-                             at + 2,
-                             parsedLines :+ bs.slice(from, at).utf8String)
-                // a CR or LF means we found a new slice
-                case CR | LF =>
-                  parseLines(bs,
-                             at + 1,
-                             at + 1,
-                             parsedLines :+ bs.slice(from, at).utf8String)
-                // for other input, simply advance
-                case _ =>
-                  parseLines(bs, from, at + 1, parsedLines)
-              }
-          buffer = parseLines(buffer ++ grab(in)) match {
-            case (remaining, _) if remaining.size > maxLineSize =>
-              failStage(
-                new IllegalStateException(
-                  s"maxLineSize of $maxLineSize exceeded!"
-                )
+      override def onPush() = {
+        @tailrec
+        def parseLines(
+            bs: ByteString,
+            from: Int = 0,
+            at: Int = 0,
+            parsedLines: Vector[String] = Vector.empty
+        ): (ByteString, Vector[String]) =
+          if (at >= bs.length || (at == bs.length - 1 && bs(at) == CR))
+            (bs.drop(from), parsedLines)
+          else
+            bs(at) match {
+              // Lookahead for LF after CR
+              case CR if bs(at + 1) == LF =>
+                parseLines(bs,
+                           at + 2,
+                           at + 2,
+                           parsedLines :+ bs.slice(from, at).utf8String)
+              // a CR or LF means we found a new slice
+              case CR | LF =>
+                parseLines(bs,
+                           at + 1,
+                           at + 1,
+                           parsedLines :+ bs.slice(from, at).utf8String)
+              // for other input, simply advance
+              case _ =>
+                parseLines(bs, from, at + 1, parsedLines)
+            }
+        buffer = parseLines(buffer ++ grab(in)) match {
+          case (remaining, _) if remaining.size > maxLineSize =>
+            failStage(
+              new IllegalStateException(
+                s"maxLineSize of $maxLineSize exceeded!"
               )
-              ByteString.empty // Clear buffer
-            case (remaining, parsedLines) =>
-              if (parsedLines.nonEmpty)
-                emitMultiple(out, parsedLines)
-              else
-                pull(in)
-              remaining
-          }
+            )
+            ByteString.empty // Clear buffer
+          case (remaining, parsedLines) =>
+            if (parsedLines.nonEmpty)
+              emitMultiple(out, parsedLines)
+            else
+              pull(in)
+            remaining
         }
-      })
+      }
 
-      setHandler(out, new OutHandler {
-        override def onPull() = pull(in)
-      })
+      override def onPull() = pull(in)
     }
 }
