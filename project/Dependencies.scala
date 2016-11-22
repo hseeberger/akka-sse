ac2913ab5de0b9fe433b76cd5a176f1a1de95426
Heiko Seeberger <hseeberger@users.noreply.github.com>
2016-11-22 17:49:01 +0100

    Upgrade to Akka HTTP 10.0.0 final (#154)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Version {
-  final val Akka       = "2.4.12"
-  final val AkkaHttp   = "10.0.0-RC2"
+  final val Akka       = "2.4.14"
+  final val AkkaHttp   = "10.0.0"
   final val Junit      = "4.12"
   final val Scala      = "2.12.0"
   final val ScalaCheck = "1.13.4"
