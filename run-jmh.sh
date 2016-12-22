121e75cc8f869aca53563b6b89072be9e6e5a765
Heiko Seeberger <mail@heikoseeberger.de>
2016-12-22 15:22:05 +0100

    Add JMH reports for v2.0.0-RC5

--- run-jmh.sh
+++ run-jmh.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-sbt "akka-sse-jmh/jmh:run -i 10 -wi 10 -f 2 -t 1" >> jmh-reports/$(git describe --tags)
+sbt "jmh/jmh:run -i 10 -wi 10 -f 2 -t 1" >> jmh-reports/$(git describe --tags)
