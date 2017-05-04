50cd59a84e6ac87394ea98662abd3e7938ba2bd3
Heiko Seeberger <mail@heikoseeberger.de>
2017-05-04 22:36:04 +0200

    Adapt to Akka HTTP package structure

---
+++ core/src/main/scala/de/heikoseeberger/akkasse/scaladsl/model/headers/headers.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015 Heiko Seeberger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.heikoseeberger.akkasse
+package scaladsl
+package model
+package headers
+
+import akka.http.scaladsl.model.headers.{ ModeledCustomHeader, ModeledCustomHeaderCompanion }
+import scala.util.Try
+
+object `Last-Event-ID` extends ModeledCustomHeaderCompanion[`Last-Event-ID`] {
+
+  override val name = "Last-Event-ID"
+
+  override def parse(value: String) = Try(new `Last-Event-ID`(value))
+}
+
+/**
+  * The Last-Event-ID header is sent by a client to the server to signal the ID of the last sever-sent event received.
+  *
+  * @param value value of the last event ID, encoded as UTF-8 string
+  */
+final case class `Last-Event-ID`(value: String)
+    extends ModeledCustomHeader[`Last-Event-ID`]
+    with javadsl.model.headers.LastEventId {
+
+  override val companion = `Last-Event-ID`
+
+  override val renderInRequests = true
+
+  override val renderInResponses = false
+
+  override def getValue = value
+}
---
+++ core/src/test/scala/de/heikoseeberger/akkasse/scaladsl/marshalling/EventStreamMarshallingSpec.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015 Heiko Seeberger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.heikoseeberger.akkasse
+package scaladsl
+package marshalling
+
+import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import akka.http.scaladsl.model.HttpRequest
+import akka.stream.scaladsl.{ Sink, Source }
+import de.heikoseeberger.akkasse.scaladsl.model.ServerSentEvent
+import org.scalatest.{ AsyncWordSpec, Matchers }
+
+final class EventStreamMarshallingSpec extends AsyncWordSpec with Matchers with AkkaSpec {
+  import EventStreamMarshalling._
+
+  "A source of ServerSentEvents" should {
+    "be marshallable to a HTTP response" in {
+      val events       = 1.to(666).map(intToServerSentEvent)
+      val marshallable = Source(events): ToResponseMarshallable
+      val response     = marshallable(HttpRequest()).flatMap(_.entity.dataBytes.runWith(Sink.seq))
+      response.map(_ shouldBe events.map(_.encode))
+    }
+  }
+
+  private def intToServerSentEvent(n: Int) = ServerSentEvent(n.toString)
+}
